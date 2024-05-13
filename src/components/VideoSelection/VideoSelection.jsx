8f189ac1dc80f73505a9aca471f812c3d0330942
Jingjing Zhu <carolzhujjz@gmail.com>
2024-05-12 23:53:38 -0700

    Sprint2 first commit

--- src/components/VideoSelection/VideoSelection.jsx
+++ src/components/VideoSelection/VideoSelection.jsx
@@ -1,32 +1,28 @@
 import "./VideoSelection.scss";
+import { Link } from "react-router-dom";
 function VideoSelection(props) {
-  console.log("selectedVideo", props.selectedVideo); //testing my prop data in the console//
-  console.log("videos: ", props.videos);
 
   return (
     <div className="selection">
       <h3 className="selection__title">NEXT VIDEOS</h3>
       {props.videos
         .filter((video) => video.id !== props.selectedVideo.id) //filters out the selected video on click
-        .map((video) => {                                       // maps through the array of objects like a for loop. 
+        .map((video) => {
+          // maps through the array of objects like a for loop.
           return (
-            <div
-              key={video.id}
-              className="selection__container"
-              onClick={() => {
-                props.handleVideoClick(video.id);   // on click takes the video id from the handle click function 
-              }}
-            >
-              <img
-                src={video.image}
-                alt="video"
-                className="selection__image"
-              ></img>
-              <div className="selection__subcontainer">
-                <h3 className="selection__header">{video.title}</h3>
-                <h4 className="selection__author">{video.channel}</h4>
+            <Link key={video.id} to={`/videos/${video.id}`} className="link">
+              <div key={video.id} className="selection__container">
+                <img
+                  src={video.image}
+                  alt="video"
+                  className="selection__image"
+                ></img>
+                <div className="selection__subcontainer">
+                  <h3 className="selection__header">{video.title}</h3>
+                  <h4 className="selection__author">{video.channel}</h4>
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
     </div>
